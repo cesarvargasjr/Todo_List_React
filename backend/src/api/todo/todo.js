fa82310f51c30cf5ad3affcab98f6cdf4de6379f
cesarvargasjr <cesar_vargas_jr@hotmail.com>
2022-03-28 19:55:42 -0300

    Módulo 7

--- backend/src/api/todo/todo.js
+++ backend/src/api/todo/todo.js
@@ -1,4 +1,4 @@
-const { Schema } = require('mongoose')
+// const { Schema } = require('mongoose')
 const restful = require('node-restful')
 const mongoose = restful.mongoose
 
--- backend/src/config/cors.js
+++ backend/src/config/cors.js
@@ -1,6 +1,6 @@
 module.exports = function(req,res, next) {
     res.header('Access-Control-Allow-Origin', '*')
     res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE, OPTIONS')
-    res.header('Access-Control-Headers', 'Origin, X-Requested-With, Content-Type, X-Requested-With, Content-Type, Accept')
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept')
     next()
 }
